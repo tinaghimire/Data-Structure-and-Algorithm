caca828e042269fcdb12b9b7aa41734b22f1ab00
tinaghimire <ghimirekristina10@gmail.com>
2023-10-14 16:01:14 +0545

    Learning Linked List

---
+++ LinkedList.py
@@ -0,0 +1,50 @@
+#Node class
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+#LinkedList class
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+
+    #Insert at beginning
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            self.tail = new_node
+            self.head = new_node
+
+    #Inser at end
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.tail.next = new_node
+            self.tail = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+            
+    #Search
+    def search(self, data):
+        current_node = self.head
+        while current_node:
+            if current_node.data == data:
+                return True
+            else:
+                current_node = current_node.next
+        return False
+    
+name = LinkedList()
+name.insert_at_beginning("Kristina")
+name.insert_at_end("Prerit")
+name.search("Gunda")
+name.search("Kristina")
+
+            
+        
---
+++ README.md
@@ -0,0 +1,22 @@
+1. Linked Lists
+
+- Sequence of data connected through links.
+- Each element, called node, has two parts: Data and pointer to the next element.
+
+Real uses: 
+- To implement other data structures:
+    -Stacks
+    -Queues
+    -Graphs
+- Access information by navigating backward and forward
+    -Web Browser
+    -Music Playlist
+
+Operations:
+1. Insert at beginning
+2. Remove at beginning
+3. Insert at end
+4. Remove at end
+5. Insert at position
+6. Remove at position
+
