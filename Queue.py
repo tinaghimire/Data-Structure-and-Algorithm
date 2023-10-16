72e9d5451ff3ba803b0b3b231741d19888c17f67
tinaghimire <ghimirekristina10@gmail.com>
2023-10-17 00:08:51 +0545

    Completed queue with code

--- Queue.py
+++ Queue.py
@@ -27,3 +27,27 @@ class Queue:
         
         if self.head == None:
             self.tail == None
+
+# Implementing a queue for printer tasks
+
+class PrinterTasks:
+  def __init__(self):
+    self.queue = Queue()
+      
+  def add_document(self, document):
+    # Add the document to the queue
+    self.queue.enqueue(document)
+      
+  def print_documents(self):
+    # Iterate over the queue while it has elements
+    while self.queue.has_elements():
+      # Remove the document from the queue
+      print("Printing", self.queue.dequeue())
+
+printer_tasks = PrinterTasks()
+# Add some documents to print
+printer_tasks.add_document("Document 1")
+printer_tasks.add_document("Document 2")
+printer_tasks.add_document("Document 3")
+# Print all the documents in the queue
+printer_tasks.print_documents()
